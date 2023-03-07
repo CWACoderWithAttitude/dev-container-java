1dd76fc6a2676734f8d3670b9f6d844867e63ffe
Volker Benders <vbenders@gmail.com>
2023-03-08 00:06:40 +0100

    wip

---
+++ src/test/java/de/baloise/devcontainer/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.baloise.devcontainer.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
